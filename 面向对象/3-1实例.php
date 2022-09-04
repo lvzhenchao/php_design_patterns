839f7a26484067d812d98ac27b2f0bb70a00c513
锐源 <1370164516@qq.com>
2022-09-04 20:43:18 +0800

    Update 3-1实例.php

--- 面向对象/3-1实例.php
+++ 面向对象/3-1实例.php
@@ -1,10 +1,13 @@
 <?php
+
 class Person {
     public $name = "lzc";
     protected $age = 22;
     private $sex = "男";
 
-    public function getName() {
+
+
+    public final function getName() {
         echo $this->name;
     }
 
@@ -20,16 +23,23 @@ class Person {
 }
 
 class P1 extends Person {
-    public function test() {
-        $this->getAge();
+
+    static $height = 175;
+    const COUNTRY = "中国";
+
+    public static function test() {
+//        $this->getAge();
 //        $this->getSex();不能访问
+        echo self::$height;//this不能访问
     }
+
 
 }
 
 $p1 = new P1();
 $p1->getName();
 $p1->test();
+echo P1::COUNTRY;
 
 $p = new Person();
 echo $p->name;
