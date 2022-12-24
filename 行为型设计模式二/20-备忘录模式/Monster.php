439d46674da38c431435f9de4750edcc3ac14e06
锐源 <1370164516@qq.com>
2022-12-24 13:32:40 +0800

    Update Monster.php

--- 行为型设计模式二/20-备忘录模式/Monster.php
+++ 行为型设计模式二/20-备忘录模式/Monster.php
@@ -6,11 +6,11 @@ class Monster{//备忘录管理器
 
     public $blood = 100;
 
-    public function setState(){
+    public function setState(){//保存当前状态
         return new Memento($this->blood);
     }
 
-    public function recoverState($memento){
+    public function recoverState($memento){//恢复状态
         $this->blood = $memento->blood;
     }
 
