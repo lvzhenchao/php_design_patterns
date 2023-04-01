f7dd2f3d7f88ea57cca0f4c93a0d491c2870d30b
锐源 <1370164516@qq.com>
2023-04-01 15:30:16 +0800

    观察者模式

---
+++ 行为型设计模式二/22-观察者模式/Normal.php
@@ -0,0 +1,13 @@
+<?php
+require_once "User.php";
+
+class Normal extends User{
+
+    public $points = 888;//可以是一个方法，去库里去查积分
+
+    public function accept($visitor)
+    {
+        // TODO: Implement accept() method.
+        $visitor->visitNormal($this);//这个参数对象，去调用方法；$this目的是为了操作vip自己积分的实际
+    }
+}
--- 行为型设计模式二/22-观察者模式/User.php
+++ 行为型设计模式二/22-观察者模式/User.php
@@ -1,4 +1,4 @@
 <?php
 abstract class User{
-    abstract public function accept($visitor);//$visitor参数代表：什么类型的观察者：普通人还是VIP用户
+    abstract public function accept($visitor);//$visitor参数代表：什么类型的观察者：普通人还是VIP用户;是个模型类。对象
 }
